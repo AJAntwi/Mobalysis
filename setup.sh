142e48a872dc1821ce4b5f455c20ddc28226c12f
cyberdev <53900439+AJAntwi@users.noreply.github.com>
2022-02-12 09:45:52 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#Write a shell script that installs postgresql, adds a user with the username mob_db_user and a password
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt install postgresql -y
-sudo su postgres
-psql -U postgres -c "CREATE ROLE mod_db_user;"
-psql -U postgres -c "ALTER USER  ubuntu  WITH PASSWORD 'mod_db_pass';"
-exit
